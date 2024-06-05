31183707d5a5446440e0116ff5b1539c111f7576
Mohsin <mohsinali1920621@gmail.com>
2024-06-05 14:07:49 +0500

    basic variable config

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345;
+
+let accountEmail = "mohsin@gmail.com";
+
+var  accountPassword = 112231;
+
+let accountCity = "Kasur";
+
+ console.log(accountId);
+ 
+console.table([accountEmail, accountPassword, accountCity])
